07a2154858bd76faeae3f8d54e24eb3fca2de5fd
4419-3-6\user <wanan850405@gmail.com>
2017-11-25 02:56:29 +0800

    VR ok

---
+++ Assets/Scripts/ShootingGunController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+using UnityEngine.VR;
+public class ShootingGunController : MonoBehaviour
+{
+    public VRInput vrInput;
+    public AudioSource audioSource;
+    public ParticleSystem flareParticle;
+    public float defaultLineLenght = 70f;
+    public LineRenderer gunFlare;
+    public float gunFareVisibleSeconds = 0.07f;
+    public Transform gunEnd;
+    private void OnEnable()
+    {
+        vrInput.OnDown += HandleDown; /*事件的用法是+=*/
+
+    }
+    private void OnDisable()
+    {
+        vrInput.OnDown -= HandleDown;
+    }
+    private void HandleDown()
+    {
+       
+        StartCoroutine (Fire());
+       
+    }
+    private IEnumerator Fire()
+    {
+        audioSource.Play();
+        float lineLength = defaultLineLenght;
+
+        flareParticle.Play();
+        gunFlare.enabled = true;
+
+        yield return StartCoroutine(MoveLineRenderer(lineLength));
+        gunFlare.enabled = false;
+    }
+    private IEnumerator MoveLineRenderer(float lineLenght)
+    {
+        float timer = 0f;
+        while(timer < gunFareVisibleSeconds)
+        {
+            gunFlare.SetPosition(0, gunEnd.position);
+            gunFlare.SetPosition(1, gunEnd.position + gunEnd.forward * lineLenght);
+            yield return null;
+            timer += Time.deltaTime;
+
+        }
+    }
+
+}
